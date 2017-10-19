00b48124a89949363aaef65dc380dc3dc2e1100f
Ravi Morbia <32268099+rmorbia@users.noreply.github.com>
2017-10-19 17:49:36 -0400

    Started JS

---
+++ Flip Switch Pillows/Morbia_Ravi_A4/js/product.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+/*** Object Constructors ***/
+
+function Pillow(name, type, shape) {
+	this.name = name;
+	this.type = type;
+	this.shape = shape;
+	this.typeImage = "";
+	this.shapeImage = "";
+	this.price = "";
+	
+}
+
+
+/*** Global Variables ***/
+var type = ;
+var shape = ;
