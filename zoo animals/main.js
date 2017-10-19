9660367318b71e54bd0385f7675a9994bb74402b
Ravi Morbia <rmorbia@cmu.edu>
2017-10-19 15:51:25 -0400

    Started layhing out the cart item

---
+++ zoo animals/main.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+function zooAnimal (type, name, weight) {
+	this.type = type;
+	this.color = color;
+	this.weight = weight;
+}
+
+function generateRandomAnimal() {
+	
+}
+
+function newAnimal() {
+	generateRandomAnimal();
+}
+
+function generateRandomColor() {
+	var colors = [red, yellow, blue];
+	var color = colors[Math.floor(Math.random()*colors.length)];
+}
+
+function generateRandomType() {
+	var colors = [red, yellow, blue];
+	var color = colors[Math.floor(Math.random()*colors.length)];
+}
